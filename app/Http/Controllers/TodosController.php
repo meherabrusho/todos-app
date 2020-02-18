2859c17f67daef32c966e244d5d58a32ec2a1391
Mohtasin Meherab Sikder <rusho1152032@gmail.com>
2020-02-18 09:50:23 +0600

    Initiating Todo app

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index(){
+        return view('todos.index')->with('todos', Todo::all());
+    }
+
+    public function show($todoId){
+        return view('todos.show')->with('todo', Todo::find($todoId));
+    }
+
+    public function create(){
+        return view('todos.create');
+    }
+
+    public function store(){
+        $this->validate(request(), [
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+        $todo = new Todo();
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->completed = false;
+    
+        $todo->save();
+
+        return redirect('/todos');
+    }
+}
